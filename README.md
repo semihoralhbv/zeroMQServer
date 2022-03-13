54990432d855f8ccd57227f48b27b7aa1bfc1067
semihoralhbv <semih.oral@hbv.edu.tr>
2022-03-13 23:04:38 +0300

    Python file addition
    Python file is created and readme file is updated.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# zeroMQServer
+# zeroMQ Server Application in Python
+
+zeroMQ server is implemented in Python.
+
+Server will read request messages from the client and reply by sorting integers in these messages.
+
+For sorting, merge sorting algorithm is used by considering time complexity.
+
+## Usage
+
+zerMQServer.py should be run and server will wait for client requests. Then, reply with sorted integer line.
---
+++ zeroMQServer.py
@@ -0,0 +1,87 @@
+import time
+import zmq
+
+
+def merge(arr, l, m, r):
+    n1 = m - l + 1
+    n2 = r - m
+
+    # create temp arrays
+    L = [0] * (n1)
+    R = [0] * (n2)
+
+    # Copy data to temp arrays L[] and R[]
+    for i in range(0, n1):
+        L[i] = arr[l + i]
+
+    for j in range(0, n2):
+        R[j] = arr[m + 1 + j]
+
+    # Merge the temp arrays back into arr[l..r]
+    i = 0  # Initial index of first subarray
+    j = 0  # Initial index of second subarray
+    k = l  # Initial index of merged subarray
+
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+
+    # Copy the remaining elements of L[], if there
+    # are any
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+
+    # Copy the remaining elements of R[], if there
+    # are any
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+
+
+# l is for left index and r is right index of the
+# sub-array of arr to be sorted
+
+
+def mergeSort(arr, l, r):
+    if l < r:
+        # Same as (l+r)//2, but avoids overflow for
+        # large l and h
+        m = l + (r - l) // 2
+
+        # Sort first and second halves
+        mergeSort(arr, l, m)
+        mergeSort(arr, m + 1, r)
+        merge(arr, l, m, r)
+
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+
+while True:
+    #  Wait for next request from client
+    message = socket.recv()
+    #print(f"Received request: {message}")
+
+    # Convert request message to integer list.
+    T1 = list(map(int, message.split()))
+
+    # Set integer numbers
+    n = len(T1)
+
+    # Apply merge sorting algorithm
+    mergeSort(T1, 0, n - 1)
+
+    # Convert sorted integer list to string format
+    strArr = ' '.join(list(map(str, T1)))
+
+    #  Send reply back to client
+    socket.send_string(strArr)
